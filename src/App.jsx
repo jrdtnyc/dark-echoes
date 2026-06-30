4755b23c79cee50753ab197c1af8b594fae3ed0a
Jearon Danglade <jdanglade82@gmail.com>
2026-06-30 00:18:15 -0400

    First commit, code is working. No CSS added yet.

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,52 @@
+import { useState } from "react";
+import { episodeList } from "./data";
+
 export default function App() {
-  // TODO
+  const [episodes] = useState(episodeList);
+  const [selectedEpisode, setSelectedEpisode] = useState();
+
+  function EpisodeDetails() {
+    if (!selectedEpisode) {
+      return (
+        <section className="details">
+          <p>Select an episode to view</p>
+        </section>
+      );
+    }
+
+    return (
+      <section className="details">
+        <h2>Episode {selectedEpisode.id}</h2>
+        <h3>{selectedEpisode.title}</h3>
+        <p>{selectedEpisode.description}</p>
+      </section>
+    );
+  }
+
+  function Episodes() {
+    return (
+      <section className="episodes">
+        <h2>Episodes</h2>
+        <ul className="episodes">
+          {episodes.map((episode) => (
+            <li key={episode.id} onClick={() => setSelectedEpisode(episode)}>
+              {episode.title}
+            </li>
+          ))}
+        </ul>
+      </section>
+    );
+  }
+
+  return (
+    <>
+      <header>
+        <h1>Dark Echoes</h1>
+      </header>
+      <main>
+        <Episodes />
+        <EpisodeDetails />
+      </main>
+    </>
+  );
 }
